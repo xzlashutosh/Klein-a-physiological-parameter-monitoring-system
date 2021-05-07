a3edda62e1ddcf8bf8802eeb69a7e68837feedac
Ashutosh Sharma <36560176+xzlashutosh@users.noreply.github.com>
2021-05-07 14:43:16 +0530

    Update klein.c

--- klein.c
+++ klein.c
@@ -62,8 +62,15 @@ volatile bool flag = 0;
 MAX30105 particleSensor;
 #define MAX_BRIGHTNESS 255
 
+#if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__)
+//Arduino Uno doesn't have enough SRAM to store 100 samples of IR led data and red led data in 32-bit format
+//To solve this problem, 16-bit MSB of the sampled data will be truncated. Samples become 16-bit data.
+uint16_t irBuffer[100]; //infrared LED sensor data
+uint16_t redBuffer[100];  //red LED sensor data
+#else
 uint32_t irBuffer[100]; //infrared LED sensor data
 uint32_t redBuffer[100];  //red LED sensor data
+#endif
 
 int32_t bufferLength; //data length
 int32_t spo2; //SPO2 value
@@ -319,7 +326,6 @@ void loop () {
             // heartRateCode
            bufferLength = 100; //buffer length of 100 stores 4 seconds of samples running at 25sps
         
-          //read the first 100 samples, and determine the signal range
           for (byte i = 0 ; i < bufferLength ; i++)
           {
             while (particleSensor.available() == false) //do we have new data?
@@ -329,10 +335,7 @@ void loop () {
             irBuffer[i] = particleSensor.getIR();
             particleSensor.nextSample(); //We're finished with this sample so move to next sample
         
-            //Serial.print(F("red="));
-            //Serial.print(redBuffer[i], DEC);
-            //Serial.print(F(", ir="));
-            //Serial.println(irBuffer[i], DEC);
+           
           }
         
           //calculate heart rate and SpO2 after first 100 samples (first 4 seconds of samples)
@@ -363,16 +366,7 @@ void loop () {
               redBuffer[i] = particleSensor.getRed();
               irBuffer[i] = particleSensor.getIR();
               particleSensor.nextSample(); //We're finished with this sample so move to next sample
-        
-              //send samples and calculation result to terminal program through UART
-              //Serial.print(F("red="));
-              //Serial.print(redBuffer[i], DEC);
-              //Serial.print(F(", ir="));
-              //Serial.print(irBuffer[i], DEC);
-
-        
-              //Serial.print(F(", HR="));
-              //Serial.print(heartRate, DEC);
+       
         
               display.setTextSize(1);
               display.setCursor(32,36);
@@ -391,14 +385,6 @@ void loop () {
               display.display();
               
               
-              //Serial.print(F(", HRvalid="));
-              //Serial.print(validHeartRate, DEC);
-        
-              //Serial.print(F(" SPO2="));
-              //Serial.print(spo2, DEC);
-        
-              //Serial.print(F(", SPO2Valid="));
-              //Serial.println(validSPO2, DEC);
             }
             
             //After gathering 25 new samples recalculate HR and SP02
@@ -407,11 +393,6 @@ void loop () {
           
           
   }
-
-
-
-  
-  
 
   
 }
@@ -432,9 +413,6 @@ void printLocalTime(){
     Serial.println("Failed to obtain time");
     return;
   }
-  //Serial.println(&timeinfo, "%A,%B %d %Y %H:%M");
-  
-  //display.clearDisplay();
   display.setTextSize(1);
   display.setCursor(8,2);
   display.println(&timeinfo, "%A, %B %d");
@@ -442,6 +420,5 @@ void printLocalTime(){
   display.setCursor(45,14);
   display.println(&timeinfo, "%H:%M");
   display.display(); 
- 
-  //Serial.println();
+
 }
